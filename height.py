e4954124bea40914d294749c570146d35469443e
Kay Lin <lamchuenkei@gmail.com>
2017-09-30 13:30:45 -0400

    Init

---
+++ height.py
@@ -0,0 +1,27 @@
+# Created by: Kay Lin
+# Created on: Sep 25th 2017
+# Created for: ICS3U
+# This program calculates the height of the object above the ground,
+#   when you give it the time
+
+import ui
+
+def calculate_touch_up_inside(sender):
+    # calculate height
+    
+    # constants
+    g = 9.81
+    
+    # input
+    time = int(view['time_textbox'].text)
+    
+    #process
+    height = 100 - 0.5 * g * (time ** 2)
+    
+    #output
+    view['answer_label'].text = 'The height is ' + str(height) + ' m'
+
+    
+
+view = ui.load_view()
+view.present('full_screen')
